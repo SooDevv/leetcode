c445fd53f45a4bcff5066d9ef27b753d055b7eb6
Soojung Kim <soojung.dev@gmail.com>
2021-04-16 11:13:54 +0900

    Add easy solution; 210416

---
+++ Solution/771_Jewels and Stones.py
@@ -0,0 +1,18 @@
+from collections import Counter
+
+
+class Solution:
+    def numJewelsInStones(self, J: str, S: str) -> int:
+        stone_count = Counter(S)
+        result = 0
+        for jewel in J:
+            if jewel in stone_count.keys():
+                result += stone_count[jewel]
+        return result
+
+
+if __name__ == '__main__':
+    jewels = "aA"
+    stones = "aAAbbbb"
+    result = Solution().numJewelsInStones(jewels, stones)
+    print(result)
