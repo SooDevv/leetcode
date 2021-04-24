76b07ffdd0f30311fbfe8da415d8c79ce0db47d5
Soojung Kim <soojung.dev@gmail.com>
2021-04-24 22:50:57 +0900

    Add easy solution; 210424

---
+++ Solution/1528_Shuffle String.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def restoreString(self, s: str, indices: List[int]) -> str:
+        str_2_idx_tuple = [(char, idx) for char, idx in zip(s, indices)]
+        str_2_idx_tuple.sort(key=lambda x: x[1])
+        result = [i[0] for i in str_2_idx_tuple]
+        return ''.join(result)
+
+
+if __name__ == '__main__':
+    s = "codeleet"
+    indices = [4, 5, 6, 7, 0, 2, 1, 3]
+    result = Solution().restoreString(s, indices)
+    assert result == 'leetcode'
