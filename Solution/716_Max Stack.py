dce40f97b1461fe4c01b82609666fb8eab47078d
Soojung Kim <soojung.dev@gmail.com>
2021-06-10 23:42:30 +0900

    Add easy solution; 210610

---
+++ Solution/716_Max Stack.py
@@ -0,0 +1,34 @@
+class MaxStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+
+    def pop(self) -> int:
+        return self.stack.pop(-1)
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def peekMax(self) -> int:
+        return max(self.stack)
+
+    def popMax(self) -> int:
+        self.stack = self.stack[::-1]
+        max_num = self.stack.pop(self.stack.index(max(self.stack)))
+        self.stack = self.stack[::-1]
+        return max_num
+
+
+# Your MaxStack object will be instantiated and called as such:
+# obj = MaxStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.peekMax()
+# param_5 = obj.popMax()
